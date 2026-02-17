0a644cad951d8bf41da7e056fb4f53e695ede5aa
CraftyElites <enochatenaga@gmail.com>
2026-02-17 18:27:34 +0100

    Serve static files from the public directory

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ const upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 const saltRounds = 10;
